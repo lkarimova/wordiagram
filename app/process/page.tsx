c5f206834ec2b732daf07e6ffcb10163b24ffba6
Liza Karimova <148022557+lkarimova@users.noreply.github.com>
2025-11-07 13:56:17 -0500

    changed UI

--- app/process/page.tsx
+++ app/process/page.tsx
@@ -23,112 +23,102 @@ export default async function ProcessPage() {
           </Link>
         </div>
 
-        <p className="text-base text-neutral-600 mb-4">
-          <span className="text-black">11/06/2025</span>
+        <p className="text-sm text-neutral-400 mb-4 leading-relaxed">
+          11/06/2025
           <br />{" "}
           <br />
           I have been learning how to code for past 3 weeks. As someone that has
           never coded before, this was a fun and challenging first project. I
           learned what API&apos;s, GitHub, Vercel, Supabase were, and how to use
           Cursor, Claude and Chat GPT to build a project that uses backend to
-          <span className="font-bold">pull in live data, process it through prompt engineering, and render
-          images representing meaning.</span> I hope you enjoy this project! Please feel free to{" "}
+          <span className="font-bold text-black"> pull in live data, process it through prompt engineering, and render
+          images representing meaning.</span> Please feel free to{" "}
           <a
             href="mailto:lkarimova.design@gmail.com"
-            className="font-bold underline underline-offset-2 hover:text-neutral-600"
+            className="font-bold text-black underline underline-offset-2 hover:text-neutral-600"
           >
             contact
           </a>{" "}
-          me with questions and feedback.
+          me with questions and feedback. I hope you enjoy this project!
         </p>
 
-        <p className="text-base text-neutral-600 mb-4">
-          <span className="font-bold text-lg text-black">Challenges:</span>
+        <p className="text-sm text-neutral-400 mb-4 leading-relaxed">
+          <span className="font-medium text-xl text-black">Challenges</span>
           <br />
           Most of the work focused on backend challenges: making the prompt
           safe, building a logic that interprets headlines as symbols rather
           than literal text, and experimenting with clustering and update
           cadence to balance coherence with variation. Getting the AI engine to
           think metaphorically instead of descriptively was surprisingly
-          difficult, since <span className="font-bold">randomness is discouraged in most models</span>, and I had
+          difficult, since <span className="font-bold text-black">randomness is discouraged in most models</span>, and I had
           to design a clear decision logic.
           <br />{" "}
           <br />
-          1.{" "}
           <span className="font-bold text-black">
-            Balancing Autonomy vs. Control in Image Generation:
+          1. Balancing Autonomy vs. Control in Image Generation:
           </span>{" "}
           Getting GPT-Image-1 to produce consistent yet symbolic paintings was
           difficult, because it tended to either over-abstract
           or over-literalize. I spent a lot of
-          effort <span className="font-bold">tuning prompts, negative prompts, and structural “locks”</span> to
+          effort <span className="font-bold text-black">tuning prompts, negative prompts, and structural “locks”</span> to
           find the right balance between freedom and compositional discipline.
+          <br />{" "}
           <br />
-          2.{" "}
           <span className="font-bold text-black">
-            Creating a Dynamic Generation Logic for Breaking News:
+          2.Creating a Dynamic Generation Logic for Breaking News:
           </span>{" "}
           Designing a stable pipeline that generated an image only when meaningful
           changes occurred was challenging. I had to define what qualified as breaking news. It included <span className="font-bold">creating thresholds
           for news recency, news cluster change, and number of news sources so the system reacted to real global shifts</span> while not over-generating images.
-          <br />
-          3.{" "}
-          <span className="font-bold text-black">
-            Prompt Composition, Safety &amp; Data Interpretation:
-          </span>{" "}
-          Linking world news clusters semantically to symbolic visual motifs demanded
-          experimentation. I started with <span className="font-bold">keyword-based clustering, but it felt too vague and generic</span>.
-          I then tried <span className="font-bold">embedding-based clustering (using text-embedding-3-small) which introduced complexity, but gave much richer results</span>. This semantic version managed to capture relationships between
-          global headlines more meaningfully. I had to <span className="font-bold">handle OpenAI safety rejections
-          by sanitizing harsh language and abstracting violent or sensitive terms</span>. I also created <span className="font-bold">fallback heuristics for edge cases through post-processing</span>, such as breaking earthquake news showing up as magnitude numbers "M 5.0" without descriptors.
-        </p>
-
-        <p className="text-base text-neutral-600 mb-4">
-          <span className="font-bold text-lg text-black">Learnings:</span>
-          <br />
-          Through this process, I learned how sensitive generative systems are
-          to framing, and <span className="font-bold">how much creativity lies in constraint</span>. It taught me
-          that <span className="font-bold">precision in logic design is what enables open-ended expression</span>.
           <br />{" "}
           <br />
-          1. It&apos;s hard to make AI not take things literally, but certain
-          things need to be defined.
+          <span className="font-bold text-black">
+          3. Prompt Composition, Safety &amp; Data Interpretation:
+          </span>{" "}
+          Linking world news clusters semantically to symbolic visual motifs demanded
+          experimentation. I started with <span className="font-bold text-black">keyword-based clustering, but it felt too vague and generic</span>.
+          I then tried <span className="font-bold text-black">embedding-based clustering (using text-embedding-3-small) which introduced complexity, but gave much richer results</span>. This semantic version managed to capture relationships between
+          global headlines more meaningfully. I had to <span className="font-bold text-black">handle OpenAI safety rejections
+          by sanitizing harsh language and abstracting violent or sensitive terms</span>. I also created <span className="font-bold text-black">fallback heuristics for edge cases through post-processing</span>, such as breaking earthquake news showing up as magnitude numbers "M 5.0" without descriptors.
+        </p>
+
+        <p className="text-sm text-neutral-400 mb-4 leading-relaxed">
+          <span className="font-medium text-xl text-black">Learnings</span>
+          <br />{" "}
           <br />
-          2. Randomness is not encouraged, so decision logic becomes extremely
+          <span className="font-bold text-black">1. Creativity lies in constraint.</span> Randomness is not encouraged, so decision logic becomes extremely
           important.
-          <br />
-          3. GPT-Image-1 has some distinct limitations<sup className="text-xs align-super">*</sup>:
-          <br />
+          <span className="font-bold text-black">2. Precision in logic design is what enables open-ended expression.</span> It&apos;s hard to make AI not take things literally, but certain
+          things need to be defined.
+          <span className="font-bold text-black">3. GPT-Image-1 has some distinct limitations:</span>
           <span className="block indent-8">
           - It cannot do additive masked image edits without regenerating the
           entire image
           </span>
-          <br />
           <span className="block indent-8">
           - It cannot derive artistic style from words without interpretation
           logic.
           </span>
-          <br />
           <span className="block indent-8">
           - It cannot apply an artistic style without modifying the content of
           the image.
           </span>
           </p>
 
-          <p className="text-base text-neutral-600 mb-4">
-          <sup className="text-xs align-super">*</sup>I originally started off with asking GPT-Image-1 to determine the
+          <p className="text-sm text-neutral-400 mb-4 leading-relaxed">
+          I originally started off with asking GPT-Image-1 to determine the
           image style based on World Art News. I also asked it to keep updating
           images with masked, additive edits based on breaking World and Art
           news. Through trial and error, I learned that those things were not
           possible, and that my design had to be simplified.
         </p>
 
-        <p className="text-base text-neutral-600 mb-4">
+        <p className="text-sm text-neutral-400 mb-4 leading-relaxed">
           Paintings from older prompt iterations are displayed below.
         </p>
 
         {processItems.length === 0 ? (
-          <p className="text-base text-neutral-600">
+          <p className="text-sm text-neutral-600">
             No earlier images to display.
           </p>
         ) : (
